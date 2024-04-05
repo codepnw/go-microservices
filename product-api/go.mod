d84d720f5a356e2d169d64488dcc8a71d2f9f4f7
codepnw <idchokms01@outlook.com>
2024-04-05 16:17:25 +0700

    start api server

---
+++ product-api/go.mod
@@ -0,0 +1,5 @@
+module github.com/codepnw/microservices/api
+
+go 1.20
+
+require github.com/nicholasjackson/env v0.6.1
---
+++ product-api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/codepnw/microservices/api/handlers"
+	"github.com/nicholasjackson/env"
+)
+
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
+func main() {
+	env.Parse()
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+	ph := handlers.NewProduct(l)
+	sm := http.NewServeMux()
+
+	sm.Handle("/", ph)
+
+	s := http.Server{
+		Addr:         *bindAddress,
+		Handler:      sm,
+		ErrorLog:     l,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		l.Println("Starting server on port :9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	
+	s.Shutdown(ctx)
+}
